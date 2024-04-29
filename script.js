3e9b265cf234a13374e2dca35e82996905da538e
Emily Comin <emilycomin@hotmail.com>
2024-04-29 20:24:32 -0300

    adicionando estilo na página de procedimento

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ document.addEventListener('scroll', function() {
  this.getElementById("ramoDireito").style.marginRight = -value + "px";
 })
  
+document.addEventListener('scroll', function() {
+    let value = window.scrollY
+    let valorFixo = 1000;
+    this.getElementById("tituloProcedimento").style.marginRight = valorFixo - value + "px";
+   })
